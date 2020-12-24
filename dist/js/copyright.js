8bcf6e02afca9ae5f0c1378887a1f62f9e5c592a
Sachin6962 <setenergy6962@gmail.com>
2020-12-24 12:21:26 +0100

    update 24 dec2020

--- dist/js/copyright.js
+++ dist/js/copyright.js
@@ -1,6 +1,5 @@
 //  huidig jaar berekenen
-var datum = new Date();
-var huidigJaar = datum.getFullYear();
+var huidigJaar = new Date().getFullYear();
 
 //  zet huidig jaar in copyright in de footer
 document.querySelector("#jaar").innerHTML = huidigJaar;
